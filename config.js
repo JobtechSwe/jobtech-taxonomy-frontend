ddb3486170aa47a3b39b5250a27fb7d6a7942a02
nymup <nyman@DESKTOP-30B140H.localdomain>
2020-04-03 08:45:04 +0200

    Updated api path

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-	api_path: "http://jobtech-taxonomy-api-for-frontend-route-jobtech-taxonomy-api.test.services.jtech.se/v1/taxonomy",
+	api_path: "http://jobtech-taxonomy-api-for-frontend-route-jobtech-taxonomy-api.test.services.jtech.se/test/v1/taxonomy",
 	api_key: "",
 };
