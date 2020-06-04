a87b588a105002338a9acbd7b38ad983c13645f5
nymup <nyman@DESKTOP-30B140H.localdomain>
2020-06-04 14:39:12 +0200

    Added filter to deprecated reference selector

--- src/view/dialog/edit_concept_deprecate.jsx
+++ src/view/dialog/edit_concept_deprecate.jsx
@@ -7,6 +7,7 @@ import Rest from './../../context/rest.jsx';
 import EventDispatcher from './../../context/event_dispatcher.jsx';
 import Localization from './../../context/localization.jsx';
 import Constants from './../../context/constants.jsx';
+import Util from './../../context/util.jsx';
 
 class EditConceptDeprecate extends React.Component { 
 
@@ -19,8 +20,33 @@ class EditConceptDeprecate extends React.Component {
             shouldReference: false,
             items: [],
             selected: null,
+            filter: "",
         };
         this.props.editContext.onSave = this.onSave.bind(this);
+    }
+
+    async fetchConcepts(type) {
+        var query = 
+            "concepts(type: \"" + type + "\", version: \"next\") { " + 
+                "id type preferredLabel:preferred_label label:preferred_label ssyk_code_2012 isco_code_08 " + 
+            "}";
+        var data = await Rest.getGraphQlPromise(query);
+        data = data.data.concepts;
+        var key = null;
+        if(type.startsWith("ssyk")) {
+            key = "ssyk_code_2012";
+        } else if(type.startsWith("isco")) {
+            key = "isco_code_08";
+        }
+        for(var i=0; i<data.length; ++i) {
+            var item = data[i];
+            item.visible = true;
+            if(key) {
+                item.label = item[key] + " - " + item.label;
+            }
+        }
+        Util.sortByKey(data, "label", true);
+        this.setState({items: data});
     }
 
     onSave(message, callback) {
@@ -64,22 +90,7 @@ class EditConceptDeprecate extends React.Component {
 
     onShouldReferenceChanged(e) {
         if(this.state.items.length == 0) {
-            Rest.getConcepts(this.props.item.type, (data) => {
-                data = data.filter((item) => {
-                    return item != null && 
-                           !item.deprecated &&
-                           item.id != this.props.item.id;
-                });
-                data.sort((a, b) => {
-                    if(a.preferredLabel < b.preferredLabel) { 
-                        return -1; 
-                    }
-                    return a.preferredLabel > b.preferredLabel ? 1 : 0;
-                });
-                this.setState({items: data});
-            }, (status) => {
-                App.showError(Util.getHttpMessage(status) + " : misslyckades hämta concept");
-            });
+            this.fetchConcepts(this.props.item.type);
         }
         if(this.state.shouldReference) {
             this.props.editContext.setEnableSave(this.state.isChanged);
@@ -92,6 +103,19 @@ class EditConceptDeprecate extends React.Component {
     onReferenceSelected(item) {
         this.props.editContext.setEnableSave(this.state.isChanged);
         this.setState({selected: item});
+    }
+
+    onFilterChanged(e) {
+        this.setState({filter: e.target.value});
+    }
+
+    onFilterClicked() {
+        var filter = this.state.filter.trim();
+        var items = this.state.items;
+        for(var i=0; i<items.length; ++i) {
+            items[i].visible = items[i].label.toLowerCase().indexOf(filter) != -1;
+        }
+        this.setState({items: items});
     }
 
     renderDeprecate() {
@@ -133,7 +157,10 @@ class EditConceptDeprecate extends React.Component {
 
     renderReferenceItems() {
         if(this.state.shouldReference) {
-            var items = this.state.items.map((item, index) => {
+            var items = this.state.items.filter((item) => {
+                return item.visible;
+            });
+            items = items.map((item, index) => {
                 var id = this.state.selected ? this.state.selected.id : null;
                 var css = id == item.id ? "deprecated_reference_selected" : "";
                 return (
@@ -141,7 +168,7 @@ class EditConceptDeprecate extends React.Component {
                         key={index}
                         className={css}
                         onMouseUp={this.onReferenceSelected.bind(this, item)}>
-                        {item.preferredLabel}
+                        {item.label}
                     </div>
                 );
             });
@@ -150,6 +177,16 @@ class EditConceptDeprecate extends React.Component {
                     <Label 
                         css="edit_concept_value_title"
                         text="Välj det begrepp som hänvisningen går till"/>
+                    <div className="edit_concept_row">
+                        <input 
+                            type="text"
+                            className="rounded"
+                            value={this.state.filter}
+                            onChange={this.onFilterChanged.bind(this)}/>
+                        <Button 
+                            text={Localization.get("filter")}
+                            onClick={this.onFilterClicked.bind(this)}/>
+                    </div>
                     <List css="deprecated_reference_list">
                         {items}
                     </List>
